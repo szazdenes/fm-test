f1c0afb1bc00d5ab0215abb856d21e3d763d5dad
szazdenes <szaz.denes@gmail.com>
2017-07-17 16:51:24 +0200

    pinmode initializations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,67 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    firmata = new QtFirmata("ttyACM0", 57600);
+
+//    connect(this, &MainWindow::signalConnected, this, &MainWindow::initialize);
+
+    connectFirmata();
+    bool available = firmata->available();
+    int isavailable = 0;
+    if(available) isavailable = 1;
+    else isavailable = 0;
+    initialize();
+    firmata->digitalWrite(allowancePin, QtFirmata::DIGITAL_HIGH);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::connectFirmata()
+{
+    bool isAvailable = false;
+    firmata->connect();
+    while(isAvailable == false){
+        isAvailable = firmata->available();
+    }
+    emit signalConnected();
+}
+
+void MainWindow::initialize()
+{
+    if(firmata->available()){
+        firmata->pinMode(panelAddressPin_1, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(panelAddressPin_2, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(panelAddressPin_3, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(panelAddressPin_4, QtFirmata::PINMODE_OUTPUT);
+
+        firmata->pinMode(photoAddressPin_1, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(photoAddressPin_2, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(photoAddressPin_3, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(photoAddressPin_4, QtFirmata::PINMODE_OUTPUT);
+
+        firmata->pinMode(measurementRangePin_1, QtFirmata::PINMODE_OUTPUT);
+        firmata->pinMode(measurementRangePin_2, QtFirmata::PINMODE_OUTPUT);
+
+        firmata->pinMode(allowancePin, QtFirmata::PINMODE_OUTPUT);
+
+        firmata->pinMode(currentLimitPin, QtFirmata::PINMODE_PWM);
+        firmata->pinMode(voltageValuePin, QtFirmata::PINMODE_PWM);
+
+        firmata->pinMode(panelCurrentMeasurePin, QtFirmata::PINMODE_ANALOG);
+        firmata->pinMode(photoValueMeasurePin, QtFirmata::PINMODE_ANALOG);
+    }
+    else{
+        qDebug("Device not available.");
+        return;
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    firmata->digitalWrite(allowancePin, QtFirmata::DIGITAL_HIGH);
+
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QEventLoop>
+
+#include "../QtFirmata/qtfirmata.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,42 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+signals:
+    void signalConnected();
+
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    static const uint8_t panelAddressPin_1 = 19;
+    static const uint8_t panelAddressPin_2 = 18;
+    static const uint8_t panelAddressPin_3 = 2;
+    static const uint8_t panelAddressPin_4 = 3;
+
+    static const uint8_t photoAddressPin_1 = 7;
+    static const uint8_t photoAddressPin_2 = 6;
+    static const uint8_t photoAddressPin_3 = 5;
+    static const uint8_t photoAddressPin_4 = 4;
+
+    static const uint8_t measurementRangePin_1 = 8;
+    static const uint8_t measurementRangePin_2 = 9;
+
+    static const uint8_t voltageValuePin = 10;
+    static const uint8_t currentLimitPin = 11;
+
+    static const uint8_t allowancePin = 13;
+
+    static const uint8_t photoValueMeasurePin = 14;
+    static const uint8_t panelCurrentMeasurePin = 15;
+
+    QtFirmata *firmata;
+
+    void connectFirmata();
+    void initialize();
+    void setPanelAddress(int panelNum);
+
 };
 
 #endif // MAINWINDOW_H
